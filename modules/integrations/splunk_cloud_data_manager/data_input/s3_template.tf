da6427f35428af5435a65af071b6a6cb6c0627e6
Ederson Brilhante <contato@edersonbrilhante.com.br>
2026-01-09 10:21:05 +0100

    fix: fix splunk DM template upload (#266)

--- modules/integrations/splunk_cloud_data_manager/data_input/s3_template.tf
+++ modules/integrations/splunk_cloud_data_manager/data_input/s3_template.tf
@@ -3,9 +3,10 @@ resource "aws_s3_object" "cloudformation_template" {
   key    = "${var.cloudformation_s3_config.key}${random_uuid.splunk_input_uuid.result}/template.json"
   source = "/tmp/${random_uuid.splunk_input_uuid.result}_template.json"
 
-  source_hash = filemd5("/tmp/${random_uuid.splunk_input_uuid.result}_template.json")
+  source_hash = sha256(jsonencode(local.tags))
 
   depends_on = [
+    null_resource.create_integration,
     data.external.splunk_dm_version,
     random_uuid.splunk_input_uuid,
   ]
