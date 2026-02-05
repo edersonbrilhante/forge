6bb3f82cad506f532a0fff7998bed5d7e71f95e4
Ederson Brilhante <contato@edersonbrilhante.com.br>
2026-02-05 17:02:21 +0100

    chore: merge renovate updates (#276)
    * chore(deps): update terraform terraform-aws-modules/lambda/aws to v8.5.0 (#87)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update pre-commit hook commitizen-tools/commitizen to v4.13.5 (#94)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update dependency hashicorp/packer to v1.15.0 (#91)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update pre-commit hook abravalheri/validate-pyproject to v0.25 (#86)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update python:3.14-slim docker digest to fa0acdc (#95)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- modules/platform/forge_runners/github_app_runner_group/main.tf
+++ modules/platform/forge_runners/github_app_runner_group/main.tf
@@ -1,6 +1,6 @@
 module "register_github_app_runner_group_lambda" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "8.4.0"
+  version = "8.5.0"
 
   function_name = "${var.prefix}-register-github-app-runner-group"
   handler       = "github_app_runner_group.lambda_handler"
