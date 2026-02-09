4aaa4d81d94d147a4c8eb2dc97d4e6e94db7a193
Ederson Brilhante <contato@edersonbrilhante.com.br>
2026-02-09 20:08:07 +0100

    feat: add new dashboard for show errors in trust relationship validation (#284)

--- modules/integrations/splunk_cloud_conf_shared/dashboard_trust_relationship.tf
+++ modules/integrations/splunk_cloud_conf_shared/dashboard_trust_relationship.tf
@@ -1,17 +1,17 @@
 locals {
-  ec2_scale_up_errors_definition = templatefile(
-    "${path.module}/template_files/ec2_scale_up_errors.json.tftpl",
+  trust_relationship_validation_definition = templatefile(
+    "${path.module}/template_files/trust_relationship_validation.json.tftpl",
     {
       splunk_index = var.splunk_conf.index,
       tenants      = var.splunk_conf.tenant_names
     }
   )
-  ec2_scale_up_errors_eai_data = <<EOF
+  trust_relationship_validation_eai_data = <<EOF
 <dashboard version="2" theme="light">
-    <label>EC2 Scale up Errors</label>
+    <label>Trust Relationship Validation</label>
     <description></description>
     <definition>
-        <![CDATA[${local.ec2_scale_up_errors_definition}]]>
+        <![CDATA[${local.trust_relationship_validation_definition}]]>
     </definition>
     <meta type="hiddenElements">
         <![CDATA[
@@ -26,9 +26,9 @@ locals {
 EOF
 }
 
-resource "splunk_data_ui_views" "ec2_scale_up_errors" {
-  name     = "ec2_scale_up_errors"
-  eai_data = local.ec2_scale_up_errors_eai_data
+resource "splunk_data_ui_views" "trust_relationship_validation" {
+  name     = "trust_relationship_validation"
+  eai_data = local.trust_relationship_validation_eai_data
 
   acl {
     app     = var.splunk_conf.acl.app
